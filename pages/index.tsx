af2b218f4a019406f76dad7acb537cb953e62abf
Test-TDP <rruk24@gmail.com>
2025-08-12 14:39:04 -0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'sans-serif' }}>
+      <h1>Kevin Speaking Agent</h1>
+      <p>Homepage is working. Try the health check API:</p>
+      <pre>/api/health</pre>
+    </main>
+  );
+}
