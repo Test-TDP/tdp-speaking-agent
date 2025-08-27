0c9e3296f583edbcee59394e72c97c1067d75fe6
Test-TDP <223160384+Test-TDP@users.noreply.github.com>
2025-08-27 12:44:30 -0500

    chore: add Tailwind and PostCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"  // ok to leave; app/ will be deleted anyway
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: { extend: {} },
   plugins: [],
